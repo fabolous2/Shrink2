346227bef18515f018eca06cb3b00890edc3adb3
sinn <160325337+fabolous2@users.noreply.github.com>
2024-06-23 13:53:42 +0300

    Update __main__.py

--- shrink/app/main/__main__.py
+++ shrink/app/main/__main__.py
@@ -36,20 +36,19 @@ async def main() -> None:
         level=logging.INFO,
         format=u'%(filename)s:%(lineno)d #%(levelname)-8s [%(asctime)s] - %(name)s - %(message)s',
     )
-    # storage = RedisStorage.from_url('redis://localhost:6379/0')
-    # jobstores = {
-    #     'default': RedisJobStore(
-    #         jobs_key='dispatched_trips_jobs',
-    #         run_times_key='dispatched_trips_running',
-    #         db=2,
-    #         host='localhost',
-    #         port=6379
-    #     )
-    # }
-    scheduler = ContextSchedulerDecorator(AsyncIOScheduler(timezone="Europe/Moscow")) #jobstores=jobstores
-# "6435829256:AAFEouFajhSIHyX08x3xaZWv2bSbnujBZTI"
+    storage = RedisStorage.from_url('redis://localhost:6379/0')
+    jobstores = {
+        'default': RedisJobStore(
+            jobs_key='dispatched_trips_jobs',
+            run_times_key='dispatched_trips_running',
+            db=2,
+            host='localhost',
+            port=6379
+        )
+    }
+    scheduler = ContextSchedulerDecorator(AsyncIOScheduler(timezone="Europe/Moscow", jobstores=jobstores)) 
     bot = Bot(token=BOT_TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
-    dispatcher = Dispatcher(scheduler=scheduler)  #storage=storage
+    dispatcher = Dispatcher(scheduler=scheduler, storage=storage) 
     scheduler.ctx.add_instance(instance=bot, declared_class=Bot)
 
     dispatcher.message.middleware.register(MailChatActionMiddleware(router=dispatcher))
